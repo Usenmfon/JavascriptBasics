4ab040940d59dbb915e07978d0aadfeeb77bc719
Usenmfon <usenmfonuko@gmail.com>
2020-12-12 21:31:31 +0100

    added an external js

---
+++ src/site.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    var elem = document.getElementsByTagName("p");
+    elem[0].innerHTML = "Hello Usenmfon uko";
+};
+
+window.onload = function() {
+    var el = document.getElementById("new");
+    el.innerHTML = "vooooooom";
+};
+
+// window.onload = function() {
+//     var e = document.getElementsByTagName("a");
+//     e[0].href = "http://www.sololearn.com";
+// }
